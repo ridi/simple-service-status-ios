1aaf522b4f69045e18e2dd511c78bdea134d3ac3
Davin Ahn <davin.ahn@ridi.com>
2019-08-05 21:53:04 +0900

    Supports Carthage

---
+++ SimpleServiceStatus-iOS/SimpleServiceStatus-iOS/SimpleServiceStatus_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SimpleServiceStatus_iOS.h
+//  SimpleServiceStatus-iOS
+//
+//  Created by Davin Ahn on 05/08/2019.
+//  Copyright © 2019 Davin Ahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleServiceStatus_iOS.
+FOUNDATION_EXPORT double SimpleServiceStatus_iOSVersionNumber;
+
+//! Project version string for SimpleServiceStatus_iOS.
+FOUNDATION_EXPORT const unsigned char SimpleServiceStatus_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleServiceStatus_iOS/PublicHeader.h>
+
+
